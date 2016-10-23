d4f39e4f789542a17588f26b9e27f0285d1f53fe
Ethan Turner <ezturner@washington.edu>
2016-10-22 17:42:03 -0700

    Added icons

--- app/src/main/java/edu/uw/scout/activities/SettingsActivity.java
+++ app/src/main/java/edu/uw/scout/activities/SettingsActivity.java
@@ -164,4 +164,15 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
