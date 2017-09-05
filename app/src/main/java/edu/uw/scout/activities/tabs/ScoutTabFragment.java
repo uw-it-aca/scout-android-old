a05ebcf24664f1648f2abf0372e9b76b40ab0478
Craig M. Stimmel <cstimmel@uw.edu>
2017-09-05 15:51:33 -0700

    Clean up if statement and logging.

--- app/src/main/java/edu/uw/scout/activities/tabs/ScoutTabFragment.java
+++ app/src/main/java/edu/uw/scout/activities/tabs/ScoutTabFragment.java
@@ -99,18 +99,13 @@ public class ScoutTabFragment extends Fragment implements TurbolinksAdapter {
     }
 
     public void reloadTab(){
-        Log.d(LOG_TAG, "reloadTab url: " + url);
         if(System.currentTimeMillis() - lastVisit <  150) {
             Log.d(LOG_TAG, "Not visiting");
             return;
         }
 
         if(!url.equals(getTabURL())){
-            Log.d(LOG_TAG, url + " != " + getTabURL());
-
-            if(url == "") {
-                url = getTabURL();
-            }
+            url = getTabURL();
 
             turbolinksSession
                     .activity(getActivity())
@@ -126,7 +121,6 @@ public class ScoutTabFragment extends Fragment implements TurbolinksAdapter {
 
     @Override
     public void onDestroy(){
-        Log.d(LOG_TAG, "onDestroy url: " + url);
         super.onDestroy();
         url = "";
         lastVisit = System.currentTimeMillis();
